3cfe97ac12096c2aca49aebe78a6facf6f4646ca
SanjayGanapathy <sanjayganap@gmail.com>
2023-10-23 17:16:09 -0700

    Lifting Mechanism and Testing for controls.
    Sanjay Ganapathy

--- TeamCode/src/main/java/org/whitneyrobotics/ftc/teamcode/Subsystems/LiftingMechanism.java
+++ TeamCode/src/main/java/org/whitneyrobotics/ftc/teamcode/Subsystems/LiftingMechanism.java
@@ -1,7 +1,10 @@
 package org.whitneyrobotics.ftc.teamcode.Subsystems;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.whitneyrobotics.ftc.teamcode.Extensions.GamepadEx.GamepadEx;
 
 public class LiftingMechanism {
     private static final double KP = 0.01; // Proportional constant for PID control
@@ -13,52 +16,49 @@ public class LiftingMechanism {
     private final DcMotorEx Motor2;
     public double errorMargin = 0.5;
 
-    public LiftingMechanism(HardwareMap hardwareMap, String motorName1, String motorName2) {
+    public LiftingMechanism(HardwareMap hardwareMap, String motorName1, String motorName2, GamepadEx gamepad2) {
         Motor1 = hardwareMap.get(DcMotorEx.class, "LiftingMotorLeft");
         Motor2 = hardwareMap.get(DcMotorEx.class, "LiftingMotorRight");
+        Motor1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE); // Ensure the motors don't move when power is 0
+        Motor2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
     }
 
     public void extendLift(double targetPosition) {
         fullyExtended = false;
         this.targetPosition = targetPosition;
+        double currentPosition = (Motor1.getCurrentPosition() + Motor2.getCurrentPosition()) / 2.0;
+        double error = targetPosition - currentPosition;
+        double power = error * KP;
 
-        while (!fullyExtended) {
-            double currentPosition = (Motor1.getCurrentPosition() + Motor2.getCurrentPosition()) / 2.0;
-            double error = targetPosition - currentPosition;
-            double power = error * KP;
-
-            if (error < errorMargin) {
-                power = 0;
-            } else {
-                if (power > MAX_POWER) {
-                    power = MAX_POWER;
-                } else if (power < -MAX_POWER) {
-                    power = -MAX_POWER;
-                }
-            }
-            Motor1.setPower(power);
-            Motor2.setPower(power);
-
-            if (Math.abs(error) < 10.0) {
-                fullyExtended = true;
-                Motor1.setPower(0.0); // Stop the motors
-                Motor2.setPower(0.0);
+        if (error < errorMargin) {
+            power = 0;
+            fullyExtended = true;
+        } else {
+            if (power > MAX_POWER) {
+                power = MAX_POWER;
+            } else if (power < -MAX_POWER) {
+                power = -MAX_POWER;
             }
         }
+
+        Motor1.setPower(power);
+        Motor2.setPower(power);
     }
 
     public void liftRobot(double targetPosition) {
-        Motor1.setPower(-MAX_POWER); // Reverse power to lift the robot
-        Motor2.setPower(-MAX_POWER);
-        fullyExtended = false;
-
-        while (!fullyExtended) {
-            double currentPosition = (Motor1.getCurrentPosition() + Motor2.getCurrentPosition()) / 2.0;
-            if (currentPosition <= targetPosition) {
-                fullyExtended = true;
-                Motor1.setPower(0.0); // Stop the motors
-                Motor2.setPower(0.0);
-            }
+        this.targetPosition = targetPosition;
+        double currentPosition = (Motor1.getCurrentPosition() + Motor2.getCurrentPosition()) / 2.0;
+        if (currentPosition <= targetPosition) {
+            Motor1.setPower(0.0);
+            Motor2.setPower(0.0);
+            fullyExtended = true;
+        } else {
+            Motor1.setPower(-MAX_POWER);
+            Motor2.setPower(-MAX_POWER);
         }
+    }
+
+    public boolean isFullyExtended() {
+        return fullyExtended;
     }
 }
