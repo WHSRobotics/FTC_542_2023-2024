6a5a550dbc7a7753fb42fbea8765205d2311bec7
Perry <perryhan0123@gmail.com>
2023-10-23 17:00:37 -0700

    Auto Paths

--- TeamCode/src/main/java/org/whitneyrobotics/ftc/teamcode/Constants/FieldConstants.java
+++ TeamCode/src/main/java/org/whitneyrobotics/ftc/teamcode/Constants/FieldConstants.java
@@ -5,6 +5,10 @@ import com.acmerobotics.roadrunner.geometry.Pose2d;
 import static org.whitneyrobotics.ftc.teamcode.Libraries.Utilities.UnitConversion.DistanceUnit.TILE_WIDTH;
 import static org.whitneyrobotics.ftc.teamcode.Constants.RobotDetails.ROBOT_LENGTH;
 public class FieldConstants {
+    public enum FieldSide {
+        BACKSTAGE, AUDIENCE
+    }
+
     public static final double BACKDROP_EDGE_FROM_WALL = 11.25;
     public static final double PIXEL_WIDTH = 3;
 
@@ -12,12 +16,12 @@ public class FieldConstants {
     public enum StartingTiles {
         BLUE_A2(new Pose2d(-TILE_WIDTH.toInches(3)+ROBOT_LENGTH/2,
                 TILE_WIDTH.toInches(-1.5),
-                0)),
+                Math.PI)),
         BLUE_A4(BLUE_A2.pose.plus(new Pose2d(0,TILE_WIDTH.toInches(2),0))),
         RED_F2(new Pose2d(
                 TILE_WIDTH.toInches(3)+ROBOT_LENGTH/2,
                 TILE_WIDTH.toInches(-1.5),
-                Math.toRadians(180)
+                Math.toRadians(0)
         )),
         RED_F4(RED_F2.pose.plus(new Pose2d(0,TILE_WIDTH.toInches(2),0)));
         public final Pose2d pose;
