44062440d63aa2504f306d956ff6a274c2e3a554
angadsbatra1 <114503992+angadsbatra1@users.noreply.github.com>
2023-10-25 17:24:19 -0700

    Update LiftingMechanism.java

--- TeamCode/src/main/java/org/whitneyrobotics/ftc/teamcode/Subsystems/LiftingMechanism.java
+++ TeamCode/src/main/java/org/whitneyrobotics/ftc/teamcode/Subsystems/LiftingMechanism.java
@@ -5,14 +5,13 @@ import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.whitneyrobotics.ftc.teamcode.Extensions.GamepadEx.GamepadEx;
-
-// Created By Sanjay Ganapathy on 10/23/2023
+ 
 
 public class LiftingMechanism {
     private static final double KP = 0.01; // Proportional constant for PID control
     private static final double MAX_POWER = 1.0; // Maximum power for the motors
 
-    private boolean fullyExtended = false;
+    private boolean fullyExtended = false; 
     private double targetPosition = 0.0;
     private final DcMotorEx Motor1;
     private final DcMotorEx Motor2;
