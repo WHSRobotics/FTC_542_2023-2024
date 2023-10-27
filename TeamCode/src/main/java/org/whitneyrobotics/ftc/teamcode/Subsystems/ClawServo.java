463463587f80f0e35a26e2254c108445c9ff35f8
quasar1742 <siddhantkhapra101@gmail.com>
2023-10-26 20:35:34 -0700

    All subsystems (Siddhant)

--- TeamCode/src/main/java/org/whitneyrobotics/ftc/teamcode/Subsystems/ClawServo.java
+++ TeamCode/src/main/java/org/whitneyrobotics/ftc/teamcode/Subsystems/ClawServo.java
@@ -1,17 +1,2 @@
-package org.whitneyrobotics.ftc.teamcode.Subsystems;
-
-import com.qualcomm.robotcore.hardware.ServoControllerEx;
-import com.qualcomm.robotcore.hardware.ServoImplEx;
-import com.qualcomm.robotcore.hardware.configuration.typecontainers.ServoConfigurationType;
-
-public class ClawServo extends ServoImplEx {
-    ClawServo(ServoControllerEx controller, int portNumber, ServoConfigurationType servoType){
-        super(controller,portNumber, servoType);
-    }
-    public static void singlePixelDrop(ClawServo ClawServoInstance) {
-        ClawServoInstance.setPosition(0.5); //Once we know the specifics, we can change 0.5 to the actual value
-    }
-    public static void doublePixelDrop(ClawServo ClawServoInstance) {
-        ClawServoInstance.setPosition(0.8); //Once we know the specifics, we can change 0.8 to the actual value
-    }
+package org.whitneyrobotics.ftc.teamcode.Subsystems;public class ClawServo {
 }
